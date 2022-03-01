3b3fffd336160bc110eb71fcf7c137823fc714cf
eshanHaque <eshan.haque16@gmail.com>
2022-03-01 21:51:07 +0600

    final finishing complete

--- js/phone.js
+++ js/phone.js
@@ -31,7 +31,7 @@ const displayMobile = (mobiles) => {
     div.classList.add('col-lg-4')
     div.innerHTML = `<div class="card p-3 shadow-lg mb-4">
     <div><img class="mx-auto d-block" src="${mobile.image}" alt=""></div>
-    <h4 class="text-center">${mobile.phone_name}</h4>
+    <h4 class="text-center fw-bold">${mobile.phone_name}</h4>
     <h5 class="text-center">${mobile.brand}</h5>
     <div class="text-center"><button onclick="details('${mobile.slug}')" class="button-color" >Detailes</button></div>
     </div>`;
@@ -53,7 +53,7 @@ const detailsMobile = (specification) => {
 
     document.getElementById("mobile-details").innerHTML = ` <div class="card p-3 shadow-lg mb-4">
     <div><img class="mx-auto d-block" src="${specification.image}" alt=""></div>
-    <h3 class="text-center mt-2 mb-1">${specification.name}</h3>
+    <h3 class="text-center mt-2 mb-1 fw-bold">${specification.name}</h3>
     <h5 class="text-center">${specification.releaseDate}</h5>
     <h5 class="text-center">Feature:</h5>
     <h5 class="text-center">Storage: <span class="ftr-font">${specification.mainFeatures.storage}</span></h5>
